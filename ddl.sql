4bd872391f93c1afc26bb5c53754807619f08f2f
ohta <ohta@raw-hide.co.jp>
2014-04-05 15:32:58 +0900

    add DDL

---
+++ ddl.sql
@@ -0,0 +1,8 @@
+CREATE DATBASE gachi;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
